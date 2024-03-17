2d941720db4305cb7f6bfb6ef2467856600c5063
Rishabh Tiwari <rishtiwari98@gmail.com>
2024-03-17 15:20:29 +0100

    created specific folders for backend and frontend

---
+++ backend/md-corpRanks/src/main/java/com/corpRanks/mdcorpRanks/MdCorpRanksApplication.java
@@ -0,0 +1,13 @@
+package com.corpRanks.mdcorpRanks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MdCorpRanksApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MdCorpRanksApplication.class, args);
+	}
+
+}
