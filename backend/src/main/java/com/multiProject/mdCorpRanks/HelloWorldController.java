84346d38215dd77103a0c1551fcdb931e16c0b34
Simon Fladen Lie <simonfl@uia.no>
2024-03-18 18:38:07 +0100

    BE Added a POST endpoint

--- backend/src/main/java/com/multiProject/mdCorpRanks/HelloWorldController.java
+++ backend/src/main/java/com/multiProject/mdCorpRanks/HelloWorldController.java
@@ -1,10 +1,20 @@
 package com.multiProject.mdCorpRanks;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
 
 @RestController
 public class HelloWorldController {
+
+    @PostMapping(path = "/alice")
+    public ResponseEntity<Default> showStatus(@RequestBody Default status){
+        return new ResponseEntity<>(status, HttpStatus.OK);
+    }
 
     @GetMapping(path = "/hello")
     public String helloWorld(){
